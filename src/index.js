d2bbd4f7668a1eb6781cab47c483434e18d142f7
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-04-23 16:00:31 +0400

    Added search bar nested component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+const API_KEY ='AIzaSyDgqbn1y19nAjHTD-_VHKTD4tI3pGlinhY';
 
 const App = () => {
 
-	return <div> Hi! </div>;
+	return(
+	 <div> 
+
+	<SearchBar />
+
+	 </div>
+	 );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
