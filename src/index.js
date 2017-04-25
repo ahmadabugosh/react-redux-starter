172a6cc5b587a3b718a133cfe11ce0cbb4d0d17d
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-04-25 09:16:53 +0400

    Implemented YouTube API call

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-const API_KEY ='AIzaSyDgqbn1y19nAjHTD-_VHKTD4tI3pGlinhY';
+const API_KEY ='AIzaSyA2fjk-titXQ43YdS-pQrF4Q7ABbICaCy4';
+import YTSearch from 'youtube-api-search';
 
-const App = () => {
 
-	return(
-	 <div> 
 
-	<SearchBar />
+class App extends Component {
+	constructor(props) {
 
-	 </div>
-	 );
+		super(props);
+		this.state ={videos:[]};
+		YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
+this.setState({videos});
+});
+	}
+
+
+	render () {
+		return(
+		 <div> 
+
+		<SearchBar />
+
+		 </div>
+		 );
+	 }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
